2ba7c78840eab9eacb2ebd350fe2434eda0a5f5b
Berk Kasapoglu <berkkasapoglu1@gmail.com>
2023-12-19 00:38:43 +0300

    feat: add and remove chip from list

--- src/components/multiselect/MultiSelect.tsx
+++ src/components/multiselect/MultiSelect.tsx
@@ -1,15 +1,41 @@
-import { useState } from 'react';
+import { ChangeEvent, KeyboardEvent, useState } from 'react';
 import classes from './MultiSelect.module.scss';
 
 function MultiSelect() {
   const [options, setOptions] = useState<string[]>([]);
+  const [value, setValue] = useState<string>('');
+
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+  };
+
+  const onKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      setOptions((prev) => [...prev, value]);
+      setValue('');
+    }
+
+    if (e.key === 'Backspace' && !value) {
+      setOptions(options.slice(0, -1));
+    }
+  };
 
   return (
     <div className={classes.container}>
-      {options.map((option) => (
-        <div className={classes.option}>{option}</div>
+      {options.map((option, idx) => (
+        <div key={idx} className={classes.option}>
+          {option}
+        </div>
       ))}
-      <input className={classes.input} />
+
+      <div className={classes.inputContainer} data-value={value}>
+        <input
+          className={classes.input}
+          onChange={onChange}
+          value={value}
+          onKeyDown={onKeyDown}
+        />
+      </div>
     </div>
   );
 }
